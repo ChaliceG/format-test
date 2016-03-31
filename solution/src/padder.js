152fe9d0def11ad110ff3b89963c9f2fa37c01d9
ChaliceG <cgreen1888@gmail.com>
2016-03-30 20:45:55 -0700

    padder not needed

--- solution/src/padder.js
+++ solution/src/padder.js
@@ -1,9 +1,22 @@
-var Padder = function (wordLength) {
-    this.wordLength = wordLength;
+var Padder = function () {
+    this.wordLength = 16;
 }
 
 Padder.prototype.pad = function (toPad) {
     return toPad;
+};
+
+Padder.prototype.buildPadString = function (stringLength) {
+    var digit = (this.wordLength - (stringLength % this.wordLength));
+    var hexDigit = digit.toString(16);
+    var i = 0;
+    var ret = ''
+
+    for(; i<digit; i++) {
+        ret = ret + hexDigit;
+    }
+
+    return ret;
 };
 
 module.exports = Padder;
--- solution/test/padder.js
+++ solution/test/padder.js
@@ -1,18 +1,49 @@
 var Padder = require('../src/padder');
 
-describe('Padder', function () {
+describe.skip('Padder', function () {
     describe('#constructor', function () {
-        it('should take a word length', function () {
-            var padder = new Padder(16);
+        it('should set the word length to 16', function () {
+            var padder = new Padder();
+
             padder.wordLength.should.equal(16);
         });
     });
     describe('#pad', function () {
         it('should take a string and return a string', function () {
             var test = 'testone';
-            var padder = new Padder(16);
+            var padder = new Padder();
 
             (typeof padder.pad(test)).should.equal('string');
+        });
+        it('should always return a longer string', function () {
+            var test = 'testone';
+            var padder = new Padder();
+
+            padder.pad(test).length.should.be.above(test.length);
+        });
+    });
+    describe('#buildPadString', function () {
+        it('should take an integer and return a string', function () {
+            var padder = new Padder();
+
+            (typeof padder.buildPadString(5)).should.equal('string');
+        });
+        [
+            {
+                length: 14,
+                padding: '22'
+            },
+            {
+                length: 16,
+                padding: 'fffffffffffffff'
+            }
+        ].forEach(testCase => {
+            it('creates a pad for str of len ' +
+                `${testCase.length} with ${testCase.padding}`, function () {
+                var padder = new Padder();
+
+                padder.buildPadString(testCase.length).should.equal(testCase.padding);
+            });
         });
     });
 });
